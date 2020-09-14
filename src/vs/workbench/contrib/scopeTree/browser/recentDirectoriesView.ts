3940488e8cc774929a95bba1eaa64ab7e29d25af
bf319 <bogdanficiu@google.com>
2020-09-14 11:30:22 +0300

    Remove unnecessary char

--- src/vs/workbench/contrib/scopeTree/browser/recentDirectoriesView.ts
+++ src/vs/workbench/contrib/scopeTree/browser/recentDirectoriesView.ts
@@ -85,7 +85,7 @@ class RecentDirectoryRenderer extends DirectoryRenderer {
 	constructor(
 		labels: ResourceLabels,
 		explorerService: IExplorerService,
-		private readonly bookmarksManager: IBookmarksManager,
+		private readonly bookmarksManager: IBookmarksManager
 	) {
 		super(labels, explorerService);
 	}
